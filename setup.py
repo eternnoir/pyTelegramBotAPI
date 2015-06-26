8df80e40ad7778fc93e4acfc5c6aebf5033b0357
eternnoir <eternnoir@gmail.com>
2015-06-26 22:44:15 +0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='pyTelegramBotAPI',
+      version='0.1',
+      description='Python Telegram bot api. ',
+      author='eternnoir',
+      author_email='eternnoir@gmail.com',
+      url='https://github.com/eternnoir/pyTelegramBotAPI',
+      packages=['VSTools'],
+      license='GPL2',
+      keywords='tools',
+      install_requires=['pytest','requests']
+      )
