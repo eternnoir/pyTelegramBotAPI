e99b4f6e649fa7a2e86c1d73a43baca222f9f26d
eternnoir <eternnoir@gmail.com>
2015-06-26 23:10:46 +0800

    Update some pip info.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='pyTelegramBotAPI',
-      version='0.1',
+      version='0.1.2',
       description='Python Telegram bot api. ',
       author='eternnoir',
       author_email='eternnoir@gmail.com',
@@ -10,5 +10,13 @@ setup(name='pyTelegramBotAPI',
       packages=['telebot'],
       license='GPL2',
       keywords='tools',
-      install_requires=['pytest','requests']
+      install_requires=['pytest', 'requests'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+      ]
       )
