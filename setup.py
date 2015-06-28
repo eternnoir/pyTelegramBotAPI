3ef164f21c3e4de187c3eb9adbc2a9f4df3c190e
eternnoir <eternnoir@gmail.com>
2015-06-28 17:37:24 +0800

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='pyTelegramBotAPI',
-      version='0.1.5',
+      version='0.1.6',
       description='Python Telegram bot api. ',
       author='eternnoir',
       author_email='eternnoir@gmail.com',
