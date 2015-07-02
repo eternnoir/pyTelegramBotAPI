21231a3f011e7563f926621accd0c329e42824e6
pieter <pevdh@live.nl>
2015-07-02 23:47:05 +0200

    PEP-8 clean-up.

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -7,6 +7,7 @@ API_TOKEN = '<api_token>'
 
 bot = telebot.TeleBot(API_TOKEN)
 
+
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help, start'])
 def send_welcome(message):
@@ -14,6 +15,7 @@ def send_welcome(message):
 Hi there, I am EchoBot.
 I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
 """)
+
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
