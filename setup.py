3a574b4596b471f84eac1d337be8d037a7235a20
eternnoir <eternnoir@gmail.com>
2015-07-08 11:36:48 +0800

    Update Version. Change log: - Thread pool support. - Add create_thread flag.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='pyTelegramBotAPI',
-      version='0.2.0',
+      version='0.2.1',
       description='Python Telegram bot api. ',
       author='eternnoir',
       author_email='eternnoir@gmail.com',
