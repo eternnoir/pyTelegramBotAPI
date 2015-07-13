0ee4fc8528119a03de259290fef6be260e7fb342
eternnoir <eternnoir@gmail.com>
2015-07-13 19:33:07 +0800

    Updare version. Change log: - add none_stop flag to avoid polling stop.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='pyTelegramBotAPI',
-      version='0.2.2',
+      version='0.2.3',
       description='Python Telegram bot api. ',
       author='eternnoir',
       author_email='eternnoir@gmail.com',
