f2664d8232f6f220ea09ef82fc31b2edad6a7f49
eternnoir <eternnoir@gmail.com>
2015-08-23 22:54:30 +0800

    Update version. Change log: - Add logging.basicConfig() to avoid handler not found.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='pyTelegramBotAPI',
-      version='0.3.2',
+      version='0.3.3',
       description='Python Telegram bot api. ',
       long_description=readme(),
       author='eternnoir',
