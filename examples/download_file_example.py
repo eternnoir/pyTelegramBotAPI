61b4ca8a37e534386a7dcda9ba3944b9b3de1e15
pieter <pevdh@live.nl>
2015-09-18 20:54:22 +0200

    Added download_file and created an example

---
+++ examples/download_file_example.py
@@ -0,0 +1,15 @@
+import telebot
+
+TOKEN = 'YOUR BOT TOKEN'
+CHAT_ID = 'YOUR CHAT ID'
+
+bot = telebot.TeleBot(TOKEN)
+
+ret_msg = bot.send_voice(CHAT_ID, open('tests/test_data/record.ogg'))
+
+file_info = bot.get_file(ret_msg.voice.file_id)
+
+downloaded_file = bot.download_file(file_info.file_path)
+
+with open('new_file.ogg', 'wb') as new_file:
+    new_file.write(downloaded_file)
