f4b0b61b9dd191367ce2513b3b60a3ed715658b9
eternnoir <eternnoir@gmail.com>
2015-09-19 23:06:47 +0800

    Update version. Change log: - Support File type. - New method getFile supported.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='pyTelegramBotAPI',
-      version='0.3.5',
+      version='0.3.6',
       description='Python Telegram bot api. ',
       long_description=readme(),
       author='eternnoir',
