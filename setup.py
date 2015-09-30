8839f367069119f74c7493555f180204cf7723dc
eternnoir <eternnoir@gmail.com>
2015-09-30 23:34:37 +0800

    Update version. Change log: - Bug fix : fix race-condition for register_for_reply. - Add timeout parameter for polling method.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='pyTelegramBotAPI',
-      version='0.3.6',
+      version='0.3.7',
       description='Python Telegram bot api. ',
       long_description=readme(),
       author='eternnoir',
