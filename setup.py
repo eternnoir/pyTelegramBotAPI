ddc1a4d66fcb670bcab9e019e77a6b8ed9ed0d2b
eternnoir <eternnoir@gmail.com>
2016-02-27 11:47:01 +0800

    Version Update. Change log: - Add disable_notification parameter. - Added setters for message/inline/chosen-inline handlers.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='pyTelegramBotAPI',
-      version='1.4.1',
+      version='1.4.2',
       description='Python Telegram bot api. ',
       long_description=readme(),
       author='eternnoir',
