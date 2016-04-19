5b234920c78fb0d9e2ec1a1a627c4c60e9a898df
eternnoir <eternnoir@gmail.com>
2016-04-19 08:19:20 +0800

    Version Update. Change log: - duplicated code remove.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='pyTelegramBotAPI',
-      version='2.0.0',
+      version='2.0.1',
       description='Python Telegram bot api. ',
       long_description=readme(),
       author='eternnoir',
