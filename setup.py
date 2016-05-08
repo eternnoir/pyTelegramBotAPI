16838c30b6185d5d81cd6efdeb76384173083b25
eternnoir <eternnoir@gmail.com>
2016-05-08 18:45:21 +0800

    Update version. Change log: - Added the field emoji to the Sticker. - Added the field forward_from_chat to the Message object for messages   forwarded from channels.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pyTelegramBotAPI',
-      version='2.0.5',
+      version='2.1.0',
       description='Python Telegram bot api. ',
       long_description=readme(),
       author='eternnoir',
