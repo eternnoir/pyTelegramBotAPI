d84aa796c03c657e47d5fc2cbac3d9cf0f5846ae
Dmitry <qwidim@yandex.ru>
2016-05-25 01:42:25 +0600

    Add files via upload

---
+++ examples/webhook_examples/webhook_flask_heroku_echo.py
@@ -0,0 +1,32 @@
+import telebot
+import os
+from flask import Flask, request
+
+bot = telebot.TeleBot('token')
+
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_messages(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8")).message
+         ])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://herokuProject_url/bot")
+    return "!", 200
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+
+server = Flask(__name__)
