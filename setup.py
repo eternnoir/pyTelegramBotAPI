303406020b2e2f8cb2fd93484b9b1858085937da
eternnoir <eternnoir@gmail.com>
2016-07-06 10:27:37 +0800

    Update version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pyTelegramBotAPI',
-      version='2.1.4',
+      version='2.1.5',
       description='Python Telegram bot api. ',
       long_description=readme(),
       author='eternnoir',
