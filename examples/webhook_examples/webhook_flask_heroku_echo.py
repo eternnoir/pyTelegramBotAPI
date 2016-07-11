3dcd59c22ef88c3757cf36b1b17e24e27ff7ec89
FrankWang <eternnoir@gmail.com>
2016-07-11 21:29:25 +0800

    Fix example

--- examples/webhook_examples/webhook_flask_heroku_echo.py
+++ examples/webhook_examples/webhook_flask_heroku_echo.py
@@ -16,8 +16,7 @@ def echo_message(message):
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
-    bot.process_new_messages(
-        [telebot.types.Update.de_json(request.stream.read().decode("utf-8")).message])
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
 @server.route("/")
