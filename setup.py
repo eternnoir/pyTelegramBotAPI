acdc2058c595b1a3a28d829d3e8ac61315ca8273
eternnoir <eternnoir@gmail.com>
2016-09-17 07:49:05 +0800

    Version change. Change log: - Fix venue's location not dejson to Location object.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pyTelegramBotAPI',
-      version='2.1.6',
+      version='2.1.7',
       description='Python Telegram bot api. ',
       long_description=readme(),
       author='eternnoir',
