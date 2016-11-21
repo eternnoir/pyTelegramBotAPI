856af7259933f37a2bb703810a562d701325f6ff
Evgeny Petrov <groosha@protonmail.com>
2016-11-21 09:19:59 +0300

    Added cache_time to answer_callback_query

--- telebot/apihelper.py
+++ telebot/apihelper.py
@@ -493,7 +493,20 @@ def get_game_high_scores(token, user_id, chat_id=None, message_id=None, inline_m
 
 # InlineQuery
 
-def answer_callback_query(token, callback_query_id, text=None, show_alert=None, url=None):
+def answer_callback_query(token, callback_query_id, text=None, show_alert=None, url=None, cache_time=None):
+    """
+    Use this method to send answers to callback queries sent from inline keyboards. The answer will be displayed to the user as a notification at the top of the chat screen or as an alert. On success, True is returned.
+    Alternatively, the user can be redirected to the specified Game URL. For this option to work, you must first create a game for your bot via BotFather and accept the terms. Otherwise, you may use links like telegram.me/your_bot?start=XXXX that open your bot with a parameter.
+    
+    :param token: Bot's token (you don't need to fill this)
+    :param callback_query_id: Unique identifier for the query to be answered
+    :param text: (Optional) Text of the notification. If not specified, nothing will be shown to the user, 0-200 characters
+    :param show_alert: (Optional) If true, an alert will be shown by the client instead of a notification at the top of the chat screen. Defaults to false.
+    :param url: (Optional) URL that will be opened by the user's client. If you have created a Game and accepted the conditions via @Botfather, specify the URL that opens your game – note that this will only work if the query comes from a callback_game button.
+    Otherwise, you may use links like telegram.me/your_bot?start=XXXX that open your bot with a parameter.
+    :param cache_time: (Optional) The maximum amount of time in seconds that the result of the callback query may be cached client-side. Telegram apps will support caching starting in version 3.14. Defaults to 0.
+    :return:
+    """
     method_url = 'answerCallbackQuery'
     payload = {'callback_query_id': callback_query_id}
     if text:
@@ -502,6 +515,8 @@ def answer_callback_query(token, callback_query_id, text=None, show_alert=None, 
         payload['show_alert'] = show_alert
     if url:
         payload['url'] = url
+    if cache_time:
+        payload['cache_time'] = cache_time
     return _make_request(token, method_url, params=payload, method='post')
 
 
