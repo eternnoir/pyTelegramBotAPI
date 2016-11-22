9f62dd04e4c3efc517dfb6b8e334582f7731da31
Areum Lee <l.areum94@gmail.com>
2016-11-22 17:08:46 +0900

    Add getNews() function

--- telebot/sejong/news.py
+++ telebot/sejong/news.py
@@ -2,19 +2,28 @@
 from bs4 import BeautifulSoup
 import urllib
 
+def getNews(command):
+    
+    # source for scraping the news
+    issue = urllib.urlopen('http://www.dailysecu.com/rss/S1N1.xml').read()
+    popular = urllib.urlopen('http://www.dailysecu.com/rss/clickTop.xml').read()
 
-r = urllib.urlopen('http://www.dailysecu.com/rss/S1N1.xml').read()
-soup = BeautifulSoup(r, "html.parser")
-news = soup.find_all("item")
+    if command == news_issue:
+        r = issue
+    elif command == news_popular:
+        r = popular
 
-i=1
-for element in news:
-    title = element.find("title").get_text()
-    description = element.find("description").get_text()
-    link = element.find("link").get_text()
-    print i
-    print "[",title,"]"         # title
-    print description+"..."     # news description
-    print "링크:",link           # link to news
-    i=i+1
+    soup = BeautifulSoup(r, "html.parser")
+    news = soup.find_all("item")
+
+    i=1
+    for element in news:
+        title = element.find("title").get_text()
+        description = element.find("description").get_text()
+        link = element.find("link").get_text()
+        print i
+        print "[",title,"]"         # title
+        print description+"..."     # news description
+        print "링크:",link           # link to news
+        i=i+1
     
