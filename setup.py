c168feea32a56ffbd7be2b949388bb5de36cf126
eternnoir <eternnoir@gmail.com>
2016-12-06 12:09:28 +0800

    Version update: Change log: - Telegram bot api 20161121 new feature. - Telegram bot api 20161204 new feature.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='pyTelegramBotAPI',
-      version='2.2.3',
+      version='2.3.0',
       description='Python Telegram bot api. ',
       long_description=readme(),
       author='eternnoir',
