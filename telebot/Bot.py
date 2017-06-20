ee188b9a251df74972c0b9cd09b21d7af413689b
paganyi <paganyi@gmail.com>
2017-06-20 23:01:41 +0300

    Create Bot.py

---
+++ telebot/Bot.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import config
+import telebot
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
