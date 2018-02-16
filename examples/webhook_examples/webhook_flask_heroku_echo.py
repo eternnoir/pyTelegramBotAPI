bc067662dc4cbbfbb4ad00787e7caa942e5a9f37
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2018-02-16 17:52:37 +0200

    Make the Heroku example actual

--- examples/webhook_examples/webhook_flask_heroku_echo.py
+++ examples/webhook_examples/webhook_flask_heroku_echo.py
@@ -1,29 +1,35 @@
-import telebot
 import os
+
+import telebot
 from flask import Flask, request
 
-bot = telebot.TeleBot('<api_token>')
-
+TOKEN = '<api_token>'
+bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
 
-@server.route("/bot", methods=['POST'])
+
+@server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
+
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://herokuProject_url/bot")
+    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
     return "!", 200
 
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-server = Flask(__name__)
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
