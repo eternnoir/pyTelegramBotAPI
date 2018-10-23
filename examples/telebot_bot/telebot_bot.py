5d80f22799412fbffe98a66ef711bbd161e55184
Vladyslav Burzakovskyy <vladislav.burzakovskij@satoshilabs.com>
2018-10-23 19:01:41 +0200

    fix: set content_types to 'new_chat_members'

--- examples/telebot_bot/telebot_bot.py
+++ examples/telebot_bot/telebot_bot.py
@@ -39,7 +39,7 @@ def is_api_group(chat_id):
     return chat_id == GROUP_CHAT_ID
 
 
-@bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_members'])
 def on_user_joins(message):
     if not is_api_group(message.chat.id):
         return
