ad92fae966269dd448177ce8bd3c153461bb551d
AkashPattanaik <akashjio66666@gmail.com>
2020-11-15 11:17:55 +0530

    Update handler_backends.py

--- telebot/handler_backends.py
+++ telebot/handler_backends.py
@@ -141,3 +141,84 @@ class RedisHandlerBackend(HandlerBackend):
             handlers = pickle.loads(value)
             self.clear_handlers(handler_group_id)
         return handlers
+    
+# The HerokuPostgress Class Support
+class HerokuPostgress:
+    """
+    The HerokuPostgress class:
+    All Methods:
+        1 -? is_user
+        2 -? add_user
+        3 -? remove_user
+        4 -? get_all_users
+
+    Explanation:
+        Method 1  >? Takes In The Chat Id And Returns True If Chat Id Is In Database Else False { String }.
+        Method 2  >? Takes In The Chat Id And Add To The Database { String }.
+        Method 3  >? Takes In The Chat Id And Removes From The Database { String }.
+        Method 4  >? Takes In Nothing Returns A List Of All Users In Database { List / Array }.
+    """
+    def __init__(self, db_url):
+        self.url = str(db_url)
+        self.all_users = []
+        if self.url.startswith('postgres://'):
+            pass
+        else:
+            os.system('')
+            print("\t  \x1B[30m\x1B[43mWarning\nInvalid Heroku PostGreSQL Url\x1B[39m\x1B[49m")
+
+        def start() -> scoped_session:
+            """
+            SQL Alchemy Scoped Session Is Returned.
+            :return:
+            """
+            engine = create_engine(self.url)
+            BASE.metadata.bind = engine
+            BASE.metadata.create_all(engine)
+            return scoped_session(
+                sessionmaker(
+                    bind=engine,
+                    autoflush=False
+                )
+            )
+        BASE = declarative_base()
+        self.SESSION = start()
+
+    class _(BASE):
+        __tablename__ = "ChatIds"
+        chat_id = Column(String(15), primary_key=True)
+
+        def __init__(self, chat_id):
+            self.chat_id = chat_id
+
+    def is_user(self, chat_id):
+        self._.__table__.create(checkfirst=True)
+        try:
+            return self.SESSION.query(self.Database).filter(self.Database.chat_id == str(chat_id)).one()
+        except Exception:
+            return False
+        finally:
+            self.SESSION.close()
+
+    def add_user(self, chat_id):
+        self._.__table__.create(checkfirst=True)
+        adder = self.Database(str(chat_id))
+        self.SESSION.add(adder)
+        self.SESSION.commit()
+
+    def remove_user(self, chat_id):
+        self._.__table__.create(checkfirst=True)
+        rem = self.SESSION.query(self.Database).get(str(chat_id))
+        if rem:
+            self.SESSION.delete(rem)
+            self.SESSION.commit()
+
+    def get_all_users(self):
+        self._.__table__.create(checkfirst=True)
+        rem = self.SESSION.query(self.Database).all()
+        self.SESSION.close()
+        self.all_users.clear()
+        for i in rem:
+            self.all_users.append(i.chat_id)
+        return self.all_users
+
