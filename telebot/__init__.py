73fb18c193691f87b784bf280e8b4bc9e5e71952
Badiboy <badialex@gmail.com>
2021-05-11 23:26:22 +0300

    Change message handler filtering order
    Now content_type is checked first.

--- telebot/__init__.py
+++ telebot/__init__.py
@@ -1880,7 +1880,7 @@ class TeleBot:
         :param commands: Optional list of strings (commands to handle).
         :param regexp: Optional regular expression.
         :param func: Optional lambda function. The lambda receives the message to test as the first parameter. It must return True if the command should handle the message.
-        :param content_types: This commands' supported content types. Must be a list. Defaults to ['text'].
+        :param content_types: Supported message content types. Must be a list. Defaults to ['text'].
         """
 
         if content_types is None:
@@ -1888,10 +1888,10 @@ class TeleBot:
 
         def decorator(handler):
             handler_dict = self._build_handler_dict(handler,
+                                                    content_types=content_types,
                                                     commands=commands,
                                                     regexp=regexp,
                                                     func=func,
-                                                    content_types=content_types,
                                                     **kwargs)
             self.add_message_handler(handler_dict)
             return handler
