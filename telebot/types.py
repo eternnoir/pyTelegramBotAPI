a9ae070256bf65c64bc78547d6f57c2aa339177a
SwissCorePy <51398037+SwissCorePy@users.noreply.github.com>
2021-06-18 22:37:31 +0200

    Update types.py

--- telebot/types.py
+++ telebot/types.py
@@ -158,7 +158,6 @@ class User(JsonDeserializable, Dictionaryable, JsonSerializable):
     def de_json(cls, json_string):
         if (json_string is None): return None
         obj = cls.check_json(json_string)
-        print (obj)
         return cls(**obj)
 
     def __init__(self, id, is_bot, first_name, last_name=None, username=None, language_code=None, 
