a4e73a05c6ea2696483b756c8e135f690f369235
Badiboy <badialex@gmail.com>
2021-06-29 13:30:01 +0300

    Update file_name to visible_file_name in send_document

--- tests/test_telebot.py
+++ tests/test_telebot.py
@@ -133,7 +133,7 @@ class TestTeleBot:
         ret_msg = tb.send_document(CHAT_ID, file_data)
         assert ret_msg.message_id
 
-        ret_msg = tb.send_document(CHAT_ID, file_data, file_name="test.jpg")
+        ret_msg = tb.send_document(CHAT_ID, file_data, visible_file_name="test.jpg")
         assert ret_msg.message_id
         
     def test_send_file_dis_noti(self):
