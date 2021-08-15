beeb60aab8171b9a426f9d239f9b3a3da41ca3fa
_run <khumogo1@gmail.com>
2021-08-15 11:40:13 +0400

    skip_updates

---
+++ examples/skip_updates_example.py
@@ -0,0 +1,15 @@
+import telebot
+
+bot = telebot.TeleBot("TOKEN")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling(skip_updates=True) # Will skip old messages when skip_updates is set
+
+# Also, you can use skip_updates in infinity_polling()
