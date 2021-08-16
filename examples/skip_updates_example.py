f4ef2366b6d73ed60cd00f616bd727546266265f
_run <khumogo1@gmail.com>
2021-08-16 22:03:17 +0400

    Update skip_updates_example.py

--- examples/skip_updates_example.py
+++ examples/skip_updates_example.py
@@ -1,6 +1,6 @@
 import telebot
 
-bot = telebot.TeleBot("TOKEN",skip_pending=True)# Skip pending skips old updates
+bot = telebot.TeleBot("TOKEN")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -10,4 +10,4 @@ def send_welcome(message):
 def echo_all(message):
 	bot.reply_to(message, message.text)
 
-bot.polling()
+bot.polling(skip_pending=True)# Skip pending skips old updates
