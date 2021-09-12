5f8c75816ebb8489224a63c8b561d24bb05dfd35
coder2020official <khumogo1@gmail.com>
2021-09-12 19:34:43 +0500

    Some useful filters
    Created useful filters that can be used in message handlers.
    Created some examples on using them.

---
+++ examples/text_filter_example.py
@@ -0,0 +1,17 @@
+import telebot
+
+bot = telebot.TeleBot('TOKEN')
+
+
+# Check if message starts with @admin tag
+@bot.message_handler(text_startswith="@admin")
+def start_filter(message):
+    bot.send_message(message.chat.id, "Looks like you are calling admin, wait...")
+
+# Check if text is hi or hello
+@bot.message_handler(text=['hi','hello'])
+def text_filter(message):
+    bot.send_message(message.chat.id, "Hi, {name}!".format(name=message.from_user.first_name))
+
+
+bot.polling(non_stop=True)
