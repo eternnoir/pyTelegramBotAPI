096d58ae71bcde06a50fa857bee1a6ca460f7389
_run <khumogo1@gmail.com>
2021-12-12 15:13:07 +0500

    Update admin_filter_example.py

--- examples/asynchronous_telebot/custom_filters/admin_filter_example.py
+++ examples/asynchronous_telebot/custom_filters/admin_filter_example.py
@@ -1,6 +1,6 @@
 from telebot.async_telebot import AsyncTeleBot
 from telebot import asyncio_filters
-bot = AsyncTeleBot('1297441208:AAGiez5xhzai5russPtPKmZjbdjybW4T0U8')
+bot = AsyncTeleBot('TOKEN')
 
 # Handler
 @bot.message_handler(chat_types=['supergroup'], is_chat_admin=True)
