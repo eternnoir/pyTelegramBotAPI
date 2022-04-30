532011138c08b0e3119d2cccaeabd6dc4281b40e
coder2020official <khumogo1@gmail.com>
2022-05-01 00:58:06 +0500

    Added examples for formatting

---
+++ examples/asynchronous_telebot/formatting_example.py
@@ -0,0 +1,52 @@
+from telebot.async_telebot import AsyncTeleBot
+from telebot import formatting
+
+bot = AsyncTeleBot('token')
+
+
+@bot.message_handler(commands=['start'])
+async def start_message(message):
+    await bot.send_message(
+        message.chat.id,
+        # function which connects all strings
+        formatting.format_text(
+            formatting.mbold(message.from_user.first_name, escape=True), # pass escape=True to escape special characters
+            formatting.mitalic(message.from_user.first_name, escape=True),
+            formatting.munderline(message.from_user.first_name, escape=True),
+            formatting.mstrikethrough(message.from_user.first_name, escape=True),
+            formatting.mcode(message.from_user.first_name, escape=True),
+            separator=" " # separator separates all strings
+        ),
+        parse_mode='MarkdownV2'
+    )
+
+    # just a bold text using markdownv2
+    await bot.send_message(
+        message.chat.id,
+        formatting.mbold(message.from_user.first_name, escape=True),
+        parse_mode='MarkdownV2'
+    )
+
+    # html
+    await bot.send_message(
+        message.chat.id,
+        formatting.format_text(
+            formatting.hbold(message.from_user.first_name, escape=True),
+            formatting.hitalic(message.from_user.first_name, escape=True),
+            formatting.hunderline(message.from_user.first_name, escape=True),
+            formatting.hstrikethrough(message.from_user.first_name, escape=True),
+            formatting.hcode(message.from_user.first_name, escape=True),
+            separator=" "
+        ),
+        parse_mode='HTML'
+    )
+
+    # just a bold text in html
+    await bot.send_message(
+        message.chat.id,
+        formatting.hbold(message.from_user.first_name, escape=True),
+        parse_mode='HTML'
+    )
+
+import asyncio
+asyncio.run(bot.polling())
