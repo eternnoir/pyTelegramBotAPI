d6ec104829d30c611ae42c12c6efda5cea7c76c2
Advik Singh Somvanshi <github@advik.dev>
2022-05-22 01:48:48 +0530

    Update create_invite_link.py
    Imported both inline keyboards in same line:|

--- examples/create_invite_link.py
+++ examples/create_invite_link.py
@@ -1,7 +1,6 @@
 import telebot
 from time import sleep, time
-from telebot.types import InlineKeyboardMarkup as ikm #Only for creating Inline Buttons, not necessary for creating Invite Links
-from telebot.types import InlineKeyboardButton as ikb #Only for creating Inline Buttons, not necessary for creating Invite Links
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton #Only for creating Inline Buttons, not necessary for creating Invite Links
 
 Token = "api_token" #Your Bot Access Token
 Group_ID = -1234567890 #Group ID for which invite link is to be created
@@ -19,8 +18,8 @@ def newmember(msg):
     #Create an invite link class that contains info about the created invite link using create_chat_invite_link() with parameters
     invite = bot.create_chat_invite_link(Group_ID, member_limit=1, expire_date=int(time())+45) #Here, the link will auto-expire in 45 seconds
     InviteLink = invite.invite_link #Get the actual invite link from 'invite' class
-    mrkplink = ikm() #Created Inline Markup Keyboard
-    mrkplink.add(ikb("Join our group 🚀", url=InviteLink)) #Added Invite Link to Inline Markup Keyboard
+    mrkplink = InlineKeyboardMarkup() #Created Inline Keyboard Markup
+    mrkplink.add(InlineKeyboardButton("Join our group 🚀", url=InviteLink)) #Added Invite Link to Inline Keyboard
     bot.send_message(msg.chat.id, f"Hey there {msg.from_user.first_name}, Click the link below to join our Official Group." reply_markup=mrkplink)
     
     #This will send a message with the newly-created invite link as markup button.
