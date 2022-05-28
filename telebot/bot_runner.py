b33f6d2a52a034de0fc9a5101f9948b2787f3729
Igor Vaiman <gosha.vaiman@gmail.com>
2022-05-28 23:52:13 +0400

    names added to handlers

--- telebot/bot_runner.py
+++ telebot/bot_runner.py
@@ -3,22 +3,21 @@ from dataclasses import dataclass, field
 from hashlib import sha256
 from typing import Coroutine
 
-from telebot import AsyncTeleBot, types
+from telebot import AsyncTeleBot
 
 
 @dataclass
 class BotRunner:
     name: str
     bot: AsyncTeleBot
-    allowed_updates: list[types.AllowedUpdateName]
     background_jobs: list[Coroutine[None, None, None]] = field(default_factory=list)
 
     def run_polling(self):
-        """For local run only"""
+        """For local run / testing only"""
 
         async def run():
             await asyncio.gather(
-                self.bot.infinity_polling(allowed_updates=self.allowed_updates, timeout=1),
+                self.bot.infinity_polling(interval=1),
                 *self.background_jobs,
             )
 
--- telebot/webhook.py
+++ telebot/webhook.py
@@ -18,23 +18,13 @@ def run_webhook_server(bot_runners: list[BotRunner], base_url: str, port: int):
         route = request.match_info.get("route")
         if route is None:
             return web.Response(status=404)
-        bot_wrapper = bot_runner_by_route.get(route)
-
-        if len(route) >= 64:
-            try:
-                logger.debug(
-                    f"Route {route}, update received:\n"
-                    + json.dumps(await request.json(), indent=2, ensure_ascii=False)
-                )
-            except Exception as e:
-                logger.debug(f"Unknwown bot path {route}, can't log update: {e}")
-
-        if bot_wrapper is None:
+        bot_runner = bot_runner_by_route.get(route)
+        if bot_runner is None:
             return web.Response(status=403)
         try:
             update = types.Update.de_json(await request.json())
             if update is not None:
-                await bot_wrapper.bot.process_new_updates([update])
+                await bot_runner.bot.process_new_updates([update])
         except Exception as e:
             update_id = update.update_id if update is not None else "<not defined>"
             logger.exception(f"Unexpected error processing update #{update_id}:\n{update}")
@@ -42,13 +32,10 @@ def run_webhook_server(bot_runners: list[BotRunner], base_url: str, port: int):
             return web.Response()
 
     async def setup_webhooks(_):
-        for route, bw in bot_runner_by_route.items():
-            await bw.bot.delete_webhook()
-            await bw.bot.set_webhook(
-                url=f"{base_url}/{route}/",
-                allowed_updates=bw.allowed_updates,
-            )
-            logger.info(f"Webhook set for {bw.name}")
+        for route, br in bot_runner_by_route.items():
+            await br.bot.delete_webhook()
+            await br.bot.set_webhook(url=f"{base_url}/{route}/")
+            logger.info(f"Webhook set for {br.name}")
 
     async def run_background_tasks(_):
         loop = asyncio.get_event_loop()
@@ -64,5 +51,5 @@ def run_webhook_server(bot_runners: list[BotRunner], base_url: str, port: int):
     app.on_startup.append(setup_webhooks)
     app.on_startup.append(run_background_tasks)
     app.on_cleanup.append(close_client_session)
-    app.router.add_post("/{route}/", handle_update)
+    app.router.add_post("/webhook/{route}/", handle_update)
     web.run_app(app, host="0.0.0.0", port=port, access_log=None)
