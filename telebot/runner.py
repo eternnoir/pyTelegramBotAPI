f55e952e142c3038ae9d667e7b3180a854936479
Igor Vaiman <gosha.vaiman@gmail.com>
2022-06-19 15:22:47 +0400

    bot runner can expose aux endpoints

--- telebot/runner.py
+++ telebot/runner.py
@@ -2,9 +2,18 @@ import asyncio
 import urllib.parse
 from dataclasses import dataclass, field
 from hashlib import sha256
-from typing import Coroutine
+from typing import Coroutine, Literal
+
+from aiohttp.typedefs import Handler as AiohttpHandler
 
 from telebot import AsyncTeleBot
+
+
+@dataclass
+class BotEndpoint:
+    method: Literal["GET", "POST", "PUT", "DELETE", "PATCH"]
+    subroute: str  # relative to baseurl/aux/, must not start with slash
+    handler: AiohttpHandler
 
 
 @dataclass
@@ -12,6 +21,7 @@ class BotRunner:
     name: str
     bot: AsyncTeleBot
     background_jobs: list[Coroutine[None, None, None]] = field(default_factory=list)
+    aux_endpoints: list[BotEndpoint] = field(default_factory=list)
 
     async def run_polling(self):
         """For local run / testing only"""
@@ -20,7 +30,10 @@ class BotRunner:
             *self.background_jobs,
         )
 
+    @property
+    def name_urlsafe(self) -> str:
+        return urllib.parse.quote("-".join(self.name.split()))
+
     def webhook_subroute(self) -> str:
-        urlasafe_name = "-".join(self.name.split())
         token_hash = sha256(self.bot.token.encode("utf-8")).hexdigest()
-        return urllib.parse.quote(f"{urlasafe_name}-{token_hash}")
+        return urllib.parse.quote(f"{self.name_urlsafe}-{token_hash}")
