87698027447bdb09db3b0ffc99f48e2d4f0cfc37
coder2020official <khumogo1@gmail.com>
2022-06-21 15:44:42 +0500

    Bump version to 4.6.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, coder2020official'
 author = 'coder2020official'
 
 # The full version, including alpha/beta/rc tags
-release = '4.5.1'
+release = '4.6.0'
 
 
 # -- General configuration ---------------------------------------------------
