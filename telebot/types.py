81cbddb8cd67368666ba0e708b60d3e9eefb6b13
Badiboy <badialex@gmail.com>
2022-07-04 22:36:42 +0300

    Added source data (json_string) to CallbackQuery

--- telebot/types.py
+++ telebot/types.py
@@ -1248,9 +1248,10 @@ class CallbackQuery(JsonDeserializable):
         obj['from_user'] = User.de_json(obj.pop('from'))
         if 'message' in obj:
             obj['message'] = Message.de_json(obj.get('message'))
+        obj['json_string'] = json_string
         return cls(**obj)
 
-    def __init__(self, id, from_user, data, chat_instance, message=None, inline_message_id=None, game_short_name=None, **kwargs):
+    def __init__(self, id, from_user, data, chat_instance, json_string, message=None, inline_message_id=None, game_short_name=None, **kwargs):
         self.id: int = id
         self.from_user: User = from_user
         self.message: Message = message
@@ -1258,6 +1259,7 @@ class CallbackQuery(JsonDeserializable):
         self.chat_instance: str = chat_instance
         self.data: str = data
         self.game_short_name: str = game_short_name
+        self.json = json_string
         
         
 class ChatPhoto(JsonDeserializable):
