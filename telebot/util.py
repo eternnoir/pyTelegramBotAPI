0aa9f0fb42ad5e4c79fa49b7c42db31c5f84bd96
Badiboy <badialex@gmail.com>
2022-07-04 22:26:24 +0300

    Update type fix

--- telebot/util.py
+++ telebot/util.py
@@ -49,7 +49,7 @@ content_type_service = [
 ]
 
 update_types = [
-    "update_id", "message", "edited_message", "channel_post", "edited_channel_post", "inline_query",
+    "message", "edited_message", "channel_post", "edited_channel_post", "inline_query",
     "chosen_inline_result", "callback_query", "shipping_query", "pre_checkout_query", "poll", "poll_answer",
     "my_chat_member", "chat_member", "chat_join_request"
 ]
