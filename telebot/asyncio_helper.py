e3f7ed8e113871bbab10441c74beef4670ef4238
byehack <khosravim861@gmail.com>
2022-09-10 16:38:56 +0430

    fix timeout

--- telebot/asyncio_helper.py
+++ telebot/asyncio_helper.py
@@ -57,29 +57,20 @@ class SessionManager:
 session_manager = SessionManager()
 
 async def _process_request(token, url, method='get', params=None, files=None, **kwargs):
-    # Let's resolve all timeout parameters.
-    # getUpdates parameter may contain 2 parameters: request_timeout & timeout.
-    # other methods may contain timeout parameter that should be applied to
-    # ClientTimeout only.
-    # timeout should be added to params for getUpdates. All other timeout's should be used
-    # for request timeout.
-    try:
-        request_timeout = kwargs.pop('request_timeout')
-        # if exception wasn't raised, then we have request_timeout in kwargs(this is getUpdates method)
-        # so we will simply apply that request_timeout to ClientTimeout
-    except KeyError:
-        # exception was raised, so we know that this method is not getUpdates.
-        # let's check for timeout in params
-        request_timeout = params.pop('timeout', None)
-        # we will apply default request_timeout if there is no timeout in params
-        # otherwise, we will use timeout parameter applied for payload.
-        request_timeout = REQUEST_TIMEOUT if request_timeout is None else request_timeout
-    
+    # timeout param just exist in getUpdates
+    if url == 'getUpdates':
+        timeout = kwargs.pop('request_timeout', None)
+    elif params:
+        timeout = params.pop('timeout', None)
+    else:
+        timeout = None
+    if timeout is None:
+        timeout = REQUEST_TIMEOUT
 
     # Preparing data by adding all parameters and files to FormData
     params = _prepare_data(params, files)
 
-    timeout = aiohttp.ClientTimeout(total=request_timeout)
+    timeout = aiohttp.ClientTimeout(total=timeout)
     got_result = False
     current_try=0
     session = await session_manager.get_session()
