7bf87a306a097f903b7785fe0281009ba9e2aaa4
_run <khumogo1@gmail.com>
2023-04-03 01:00:48 +0500

    Update redis_storage.py

--- telebot/asyncio_storage/redis_storage.py
+++ telebot/asyncio_storage/redis_storage.py
@@ -170,6 +170,6 @@ class StateRedisStorage(StateStorageBase):
         user_id = str(user_id)
         if response:
             if user_id in response:
-                response[user_id]['data'] = dict(data, **response[user_id]['data'])
+                response[user_id]['data'] = data
                 await self.set_record(chat_id, response)
                 return True
