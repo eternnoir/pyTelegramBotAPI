8fee13649079014242826bb11144fbc51ab31f7d
_run <khumogo1@gmail.com>
2023-12-29 19:28:47 +0500

    Fix tests for the new handler

--- tests/test_handler_backends.py
+++ tests/test_handler_backends.py
@@ -65,9 +65,10 @@ def update_type(message):
     my_chat_member = None
     chat_member = None
     chat_join_request = None
+    message_reaction = None
     return types.Update(1001234038283, message, edited_message, channel_post, edited_channel_post, inline_query,
                         chosen_inline_result, callback_query, shipping_query, pre_checkout_query, poll, poll_answer,
-                        my_chat_member, chat_member, chat_join_request)
+                        my_chat_member, chat_member, chat_join_request, message_reaction)
 
 
 @pytest.fixture()
@@ -85,9 +86,10 @@ def reply_to_message_update_type(reply_to_message):
     my_chat_member = None
     chat_member = None
     chat_join_request = None
+    message_reaction = None
     return types.Update(1001234038284, reply_to_message, edited_message, channel_post, edited_channel_post,
                         inline_query, chosen_inline_result, callback_query, shipping_query, pre_checkout_query, 
-                        poll, poll_answer, my_chat_member, chat_member, chat_join_request)
+                        poll, poll_answer, my_chat_member, chat_member, chat_join_request, message_reaction)
 
 
 def next_handler(message):
