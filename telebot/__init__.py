778a02c45b3291dca447d1cecc395d0c9de45b69
Badiboy <badialex@gmail.com>
2024-01-03 00:45:46 +0300

    Fix forward_messages return value

--- telebot/__init__.py
+++ telebot/__init__.py
@@ -1879,8 +1879,8 @@ class TeleBot:
         disable_notification = self.disable_notification if (disable_notification is None) else disable_notification
         protect_content = self.protect_content if (protect_content is None) else protect_content
 
-        return types.MessageID.de_json(
-            apihelper.forward_messages(self.token, chat_id, from_chat_id, message_ids, disable_notification, protect_content, message_thread_id))
+        return [types.MessageID.de_json(message_id) for message_id in
+            apihelper.forward_messages(self.token, chat_id, from_chat_id, message_ids, disable_notification, protect_content, message_thread_id))]
     
     def copy_messages(self, chat_id: Union[str, int], from_chat_id: Union[str, int], message_ids: List[int],
                         disable_notification: Optional[bool] = None, message_thread_id: Optional[int] = None,
