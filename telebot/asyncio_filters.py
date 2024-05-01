25f97213793818662ad80104a202ff6c12e2fe59
Badiboy <badialex@gmail.com>
2024-05-01 20:44:04 +0300

    Revert add type hints to SimpleCustomFilter

--- telebot/asyncio_filters.py
+++ telebot/asyncio_filters.py
@@ -27,7 +27,7 @@ class SimpleCustomFilter(ABC):
 
     key: str = None
 
-    async def check(self, message: types.Message) -> bool:
+    async def check(self, message) -> bool:
         """
         Perform a check.
         """
