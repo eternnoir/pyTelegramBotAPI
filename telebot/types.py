01e92b1a49c3441b4c32f5c6b71600809fd1759a
_run <khumogo1@gmail.com>
2024-07-01 22:57:24 +0500

    Added the field can_send_paid_media to the class ChatFullInfo.

--- telebot/types.py
+++ telebot/types.py
@@ -707,6 +707,10 @@ class ChatFullInfo(JsonDeserializable):
     :param location: Optional. For supergroups, the location to which the supergroup is connected. Returned only in getChat.
     :type location: :class:`telebot.types.ChatLocation`
 
+    :param can_send_paid_media: Optional. True, if paid media messages can be sent or forwarded to the channel chat.
+        The field is available only for channel chats.
+    :type can_send_paid_media: :obj:`bool`
+
     :return: Instance of the class
     :rtype: :class:`telebot.types.ChatFullInfo`
     """
@@ -748,7 +752,8 @@ class ChatFullInfo(JsonDeserializable):
                  available_reactions=None, accent_color_id=None, background_custom_emoji_id=None, profile_accent_color_id=None,
                  profile_background_custom_emoji_id=None, has_visible_history=None, 
                  unrestrict_boost_count=None, custom_emoji_sticker_set_name=None, business_intro=None, business_location=None,
-                    business_opening_hours=None, personal_chat=None, birthdate=None, **kwargs):
+                    business_opening_hours=None, personal_chat=None, birthdate=None, 
+                    can_send_paid_media=None, **kwargs):
         self.id: int = id
         self.type: str = type
         self.title: str = title
@@ -792,6 +797,7 @@ class ChatFullInfo(JsonDeserializable):
         self.business_opening_hours: BusinessOpeningHours = business_opening_hours
         self.personal_chat: Chat = personal_chat
         self.birthdate: Birthdate = birthdate
+        self.can_send_paid_media: bool = can_send_paid_media
 
 
 class Chat(ChatFullInfo):
