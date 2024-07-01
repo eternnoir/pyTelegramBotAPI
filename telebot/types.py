5117807ea11ae97658dbfba4f772f8685ff58ef8
_run <khumogo1@gmail.com>
2024-07-01 23:13:51 +0500

    Added support for launching Web Apps via t.me link in the class MenuButtonWebApp.

--- telebot/types.py
+++ telebot/types.py
@@ -7543,7 +7543,9 @@ class MenuButtonWebApp(MenuButton):
     :type text: :obj:`str`
 
     :param web_app: Description of the Web App that will be launched when the user presses the button. The Web App will be 
-        able to send an arbitrary message on behalf of the user using the method answerWebAppQuery.
+        able to send an arbitrary message on behalf of the user using the method answerWebAppQuery. Alternatively, a t.me link
+        to a Web App of the bot can be specified in the object instead of the Web App's URL, in which case the Web App will be
+        opened as if the user pressed the link.
     :type web_app: :class:`telebot.types.WebAppInfo`
 
     :return: Instance of the class
