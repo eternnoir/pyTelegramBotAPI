adbf425e0253233a2b265b33dc2b61dbad50d056
nypava <ny.dev0.em@gmail.com>
2024-08-04 15:00:22 +0300

    add features to webapp

--- examples/webapp/bot.py
+++ examples/webapp/bot.py
@@ -1,16 +1,27 @@
 from telebot import TeleBot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from telebot.types import (
+    ReplyKeyboardMarkup, 
+    KeyboardButton, 
+    WebAppInfo,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 
-BOT_TOKEN = '<bot_token>' 
-WEB_URL = 'https://pytelegrambotminiapp.vercel.app/' # https://github.com/eternnoir/pyTelegramBotAPI/blob/master/examples/webapp/webapp.html
+BOT_TOKEN = "" 
+WEB_URL = "https://pytelegrambotminiapp.vercel.app" # https://github.com/eternnoir/pyTelegramBotAPI/tree/master/examples/webapp/webapp
 
 bot = TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    reply_markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    reply_markup.row(KeyboardButton("Start MiniApp", web_app=WebAppInfo(WEB_URL)))
-    bot.reply_to(message, "Click the button to start MiniApp", reply_markup=reply_markup)
+    reply_keyboard_markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    reply_keyboard_markup.row(KeyboardButton("Start MiniApp", web_app=WebAppInfo(WEB_URL)))
+
+    inline_keyboard_markup = InlineKeyboardMarkup()
+    inline_keyboard_markup.row(InlineKeyboardButton('Start MiniApp', web_app=WebAppInfo(WEB_URL)))
+
+    bot.reply_to(message, "Click the bottom inline button to start MiniApp", reply_markup=inline_keyboard_markup)
+    bot.reply_to(message, "Click keyboard button to start MiniApp", reply_markup=reply_keyboard_markup)
 
 @bot.message_handler(content_types=['web_app_data'])
 def web_app(message):
