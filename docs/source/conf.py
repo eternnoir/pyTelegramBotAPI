dd30e78dd45c53165f579d089c5cb9e76d213a27
Badiboy <badialex@gmail.com>
2024-08-17 22:46:45 +0300

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ author = 'coder2020official'
 copyright = f'2022-{datetime.now().year}, {author}'
 
 # The full version, including alpha/beta/rc tags
-release = '4.22.1'
+release = '4.23.0'
 
 
 # -- General configuration ---------------------------------------------------
