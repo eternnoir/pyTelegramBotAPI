d6a159ec1d5b21a9e8488fc8c70c68573512965f
Igor Vaiman <igor.vaiman@inten.to>
2025-03-10 16:31:52 +0400

    shutdown timeout added

--- telebot/graceful_shutdown.py
+++ telebot/graceful_shutdown.py
@@ -1,8 +1,10 @@
 import asyncio
 import contextlib
+import datetime
 import functools
 import logging
 import signal
+import time
 from types import FrameType
 from typing import Awaitable, Callable, TypeVar
 from weakref import WeakSet
@@ -51,8 +53,6 @@ class PreventShutdown(GracefulShutdownCondition):
     ...         await update_users(updated_users)
     ...         async with prevent_shutdown.allow_shutdown():
     ...             await asyncio.sleep(120)
-
-
     """
 
     def __init__(self, reason: str):
@@ -60,7 +60,7 @@ class PreventShutdown(GracefulShutdownCondition):
         self._reason = reason
         super().__init__(
             predicate=self.is_ready_to_shutdown,
-            description=f"Preventing shutdown, {self._reason}",
+            description=f"{self.__class__.__name__}({self._reason!r})",
         )
 
     async def is_ready_to_shutdown(self) -> bool:
@@ -106,17 +106,17 @@ class GracefulShutdownHandler:
         return cls._instance
 
     def manual_init(self) -> None:
-        self._is_shutting_down = False
+        self._shutdown_start_time: float | None = None
         self._is_running = False
 
     def _shutdown_signal_handler(self, sig: int, frame: FrameType | None):
-        if not self._is_shutting_down:
+        if self._shutdown_start_time is None:
             logger.info(f"Shutdown signal received: {signal.Signals(sig).name}, entering shutdown state")
-            self._is_shutting_down = True
+            self._shutdown_start_time = time.time()
         else:
             logger.info(f"Repeated shutdown signal received: {signal.Signals(sig).name}, ignoring")
 
-    async def run(self):
+    async def run(self, shutdown_timeout: datetime.timedelta | None = None):
         if self._is_running:
             raise RuntimeError("Graceful shutdown handler may be run only once")
         self._is_running = True
@@ -124,17 +124,24 @@ class GracefulShutdownHandler:
         signal.signal(signal.SIGTERM, self._shutdown_signal_handler)
         while True:
             await asyncio.sleep(1.0)
-            if not self._is_shutting_down:
+            if self._shutdown_start_time is None:
                 continue
 
+            is_ready_to_shutdown = True
             for condition in GracefulShutdownCondition.instances:
                 if not await condition.is_ready():
-                    logger.info(f"Shutdown condition is not satisfied yet, waiting: {condition.description!r}")
-                    break
-            else:
-                logger.info("All shutdown conditions are satisfied, shutting down! See ya.")
+                    logger.info(f"Shutdown condition is not satisfied yet, waiting: {condition.description}")
+                    is_ready_to_shutdown = False
+
+            if is_ready_to_shutdown:
+                logger.info("All shutdown conditions are satisfied! See ya.")
+                raise SystemExit()
+
+            shutting_down_for = time.time() - self._shutdown_start_time
+            if shutdown_timeout is not None and shutting_down_for > shutdown_timeout.total_seconds():
+                logger.info(f"Shutdown timeout ({shutdown_timeout}) reached: {shutting_down_for:.3f} sec")
                 raise SystemExit()
 
 
 def is_shutting_down() -> bool:
-    return GracefulShutdownHandler()._is_shutting_down
+    return GracefulShutdownHandler()._shutdown_start_time is not None
