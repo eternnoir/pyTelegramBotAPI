bcf258ed369e4b8072671ed629d530e5152980ca
Hikmalyogapratama <91348953+Hikmalyogapratama@users.noreply.github.com>
2025-11-06 05:49:31 +0700

    Change bot name and update API token
    Updated bot name and API token in detect_changes.py.

--- examples/detect_changes.py
+++ examples/detect_changes.py
@@ -5,17 +5,17 @@
 
 import telebot
 
-API_TOKEN = '<api_token>'
+API_TOKEN = '<8386982089:AAE_167l-UrzCINv5mjUEfxbEmaGu8XFfyo>'
 
-bot = telebot.TeleBot(API_TOKEN)
+bot = telebot.TeleBot(8386982089:AAE_167l-UrzCINv5mjUEfxbEmaGu8XFfyo)
 
 
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+Hi there, I am Bot Mereun.
+I am here to your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
 """)
 
 
