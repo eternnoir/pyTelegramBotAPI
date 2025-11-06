0474237a40a86c3c1d151ca2d39bc5de73518ef2
Hikmalyogapratama <91348953+Hikmalyogapratama@users.noreply.github.com>
2025-11-06 12:41:56 +0700

    Implement basic echo bot using Telebot
    Signed-off-by: Hikmalyogapratama <91348953+Hikmalyogapratama@users.noreply.github.com>

---
+++ echo_bot.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("8386982089:AAE_167l-UrzCINv5mjUEfxbEmaGu8XFfyo)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
